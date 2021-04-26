3735b55276d70a787a368a03a639ec7aefdc87ff
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-04-26 23:28:53 +0200

    Add Cook abstract class hpp

---
+++ src/Plazza/Cook/Cook.hpp
@@ -0,0 +1,27 @@
+/*
+ * EPITECH PROJECT, 2021
+ * Cook
+ * File description:
+ * Cook.hpp - Created: 26/04/2021
+ */
+
+#ifndef COOK_HPP
+#define COOK_HPP
+
+#include "Interfaces/CookInterface.hpp"
+
+class Cook : ICook {
+  public:
+    ~ICook();
+    bool cook(Pizza);
+    Pizza getCookingPizza() const;
+    Pizza getPendingPizza() const;
+    std::queue<Pizza> getFinishedPizzas() const;
+
+  private:
+    Pizza _cooking;
+    Pizza _pending;
+    std::queue<Pizza> _finished;
+};
+
+#endif
