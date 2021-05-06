9aa9e899e7a07460ca1683b73cf814d500c7679d
arthur.jourdan <arthur.jourdan@epitech.eu>
2021-05-06 10:14:59 +0200

    refactor(LockedQueue.?pp): put implementation of class in header cause template

--- src/Utils/Encapsulations/LockedQueue/LockedQueue.hpp
+++ src/Utils/Encapsulations/LockedQueue/LockedQueue.hpp
@@ -13,12 +13,23 @@
 template <typename T> class LockedQueue : public std::queue<T> {
   public:
     LockedQueue() = default;
-    explicit LockedQueue(const std::queue<T> &queue);
+    explicit LockedQueue(const std::queue<T> &queue) : std::queue<T>(queue){};
     ~LockedQueue() = default;
 
-    void pop(T elem) override;
-    void push(T elem) override;
-    [[nodiscard]] bool empty() const override;
+    void pop(T elem) override
+    {
+        std::lock_guard<std::mutex> my_lock(_mutex);
+        std::queue<T>::pop(elem);
+    };
+    void push(T elem) override
+    {
+        std::lock_guard<std::mutex> my_lock(_mutex);
+        std::queue<T>::push(elem);
+    };
+    [[nodiscard]] bool empty() const override
+    {
+        return std::queue<T>::empty();
+    };
 
   private:
     std::mutex _mutex;
