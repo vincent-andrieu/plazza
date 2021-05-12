f492b482a0e798ca974d2713f1804f779f30d5a2
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-05-12 17:22:57 +0200

    Fix arguments error manage

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,19 @@
 #include "Plazza.hpp"
 #include "Restaurant/Restaurant.hpp"
 
-void printHelp(const string &binaryName)
+static void printHelp(const string &binaryName)
 {
     std::cout << "USAGE:\t" << binaryName << " COOKING_TIME COOKS STOCK_RELOAD" << std::endl
               << "\tCOOKING_TIME:\tMultiplier for the cooking time." << std::endl
               << "\tCOOKS:\t\tNumber of cooks per kitchen." << std::endl
               << "\tSTOCK_RELOAD:\tTime in milliseconds, used by the kitchen stock to replace ingredients." << std::endl;
+}
+
+static bool isNumber(const string &str)
+{
+    return !str.empty() && std::find_if(str.begin(), str.end(), [](unsigned char c) {
+        return !std::isdigit(c);
+    }) == str.end();
 }
 
 int main(int argc, char **argv)
@@ -24,10 +31,14 @@ int main(int argc, char **argv)
         return argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) ? EXIT_SUCCESS : EXIT_ERROR;
     }
 
-    double bakingMultiplier = atof(argv[1]);
+    if (!isNumber(argv[1]) || !isNumber(argv[2]) || !isNumber(argv[3]))
+        return EXIT_ERROR;
+
+    double bakingMultiplier;
     size_t cooksPerKitchen;
     double restockTime;
 
+    stringstream(argv[1]) >> bakingMultiplier;
     stringstream(argv[2]) >> cooksPerKitchen;
     stringstream(argv[3]) >> restockTime;
     if (bakingMultiplier == 0.0)
